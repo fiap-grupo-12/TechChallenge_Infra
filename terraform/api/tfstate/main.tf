b1fd378ea0bd9d904b4a04e83a410ff6887977dd
José Henrique <henriquesantoscjr2@hotmail.com>
2024-09-15 22:09:34 -0300

    Ajuste terraform

--- terraform/api/tfstate/main.tf
+++ terraform/api/tfstate/main.tf
@@ -7,10 +7,3 @@ resource "aws_s3_bucket" "s3_tfstate" {
   acl    = "private"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-tfstate-grupo12-fiap-2024"
-    key    = "infra/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
